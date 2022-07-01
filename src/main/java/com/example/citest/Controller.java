3c7a14de86d7ed64ed22d7065794bae2deb472b8
seungchanKKK <eccsck@gmail.com>
2022-07-01 14:40:18 +0900

    문자열 체크 테스트

---
+++ src/main/java/com/example/citest/Controller.java
@@ -0,0 +1,13 @@
+package com.example.citest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class Controller {
+    @GetMapping("/tests")
+    public String test(){
+        return "테스트문자";
+    }
+}
