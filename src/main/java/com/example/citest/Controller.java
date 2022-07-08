5c66327339996ba069235582dd65980d5bd6f8ec
seungchanKKK <eccsck@gmail.com>
2022-07-08 23:49:08 +0900

    dd

--- src/main/java/com/example/citest/Controller.java
+++ src/main/java/com/example/citest/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Controller {
     @GetMapping("/tests")
+
     public String test(){
         return "테스트문자 yml 좀되라";
     }
---
+++ src/main/java/com/example/citest/asd.java
@@ -0,0 +1,7 @@
+package com.example.citest;
+
+import java.util.List;
+
+public class asd {
+    List<String>asd = 
+}
